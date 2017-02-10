1146bc6e415edb432a4f7c1af9c4a4c113d37cc6
chybie <chy.bie@htmail.com>
2017-02-10 23:41:55 +0800

    extract base url

--- src/app/template-driven-file-upload/file-upload.service.ts
+++ src/app/template-driven-file-upload/file-upload.service.ts
@@ -4,15 +4,17 @@ import { Http, RequestOptionsArgs, Headers } from '@angular/http';
 @Injectable()
 export class FileUploadService {
 
+    baseUrl = 'http://localhost:3000';
+
     constructor(private _http: Http) { }
 
     upload(formData) {
-        const url = 'http://localhost:3001/photos/upload';
+        const url = `${this.baseUrl}/photos/upload`;
         return this._http.post(url, formData)
             .map(x => x.json())
             .map((x: any[]) => x
                 .map(item => Object
-                    .assign({}, item, { url: `http://localhost:3001/images/${item.id}` }))
+                    .assign({}, item, { url: `${this.baseUrl}/images/${item.id}` }))
             );
     }
 }

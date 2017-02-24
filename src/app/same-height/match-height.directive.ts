456c95012da7c1b88ce1428122b0e00a5d99510d
setrar <setrar@valiha.com>
2017-02-23 22:32:54 -0500

    Update match-height.directive.ts

--- src/app/same-height/match-height.directive.ts
+++ src/app/same-height/match-height.directive.ts
@@ -29,27 +29,27 @@ export class MatchHeightDirective implements AfterViewChecked {
     matchHeight(parent: HTMLElement, className: string) {
         // match height logic here
 
-        if (!parent) return;
-        const children = parent.getElementsByClassName(className);
+      if (parent) {
 
-        if (!children) return;
+        let children: Element[] = Array.from(parent.getElementsByClassName(className));
+        if (children) {
 
-        // reset all children height
-        Array.from(children).forEach((x: HTMLElement) => {
-            x.style.height = 'initial';
-        })
+          // reset all children height
+          children.forEach((x: HTMLElement) => x.style.height = 'initial');
 
-        // gather all height
-        const itemHeights = Array.from(children)
-            .map(x => x.getBoundingClientRect().height);
+          // gather all height
+          let itemHeights: number[] = children.map(x => x.getBoundingClientRect().height);
 
-        // find max height
-        const maxHeight = itemHeights.reduce((prev, curr) => {
-            return curr > prev ? curr : prev;
-        }, 0);
+          // find max height
+          let maxHeight: number = itemHeights.reduce((prev, curr) => curr > prev ? curr : prev, 0);
 
-        // apply max height
-        Array.from(children)
-            .forEach((x: HTMLElement) => x.style.height = `${maxHeight}px`);
+          // apply max height
+          children.forEach((x: HTMLElement) => x.style.height = `${maxHeight}px`);
+
+        }
+
+      }
+      
     }
+
 }
